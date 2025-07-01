0055925cdc6d6872bcc7204c21b14f9d8394bfe4
Huy Nguyen <beshin81@gmail.com>
2025-07-01 15:38:50 +0700

    Implement findMissingLetter function and add corresponding tests

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,22 @@
-function findMissingLetter() {}
+// function findMissingLetter(letters) {
+//   if (!letters.length) return '';
+//   const len = letters.length;
+//   const letterArr = letters.map(val => val.charCodeAt(0));
+//   const expectedSum = ((letterArr[len - 1] + letterArr[0]) * (len + 1)) / 2;
+//   const actualSum = letterArr.reduce((sum, num) => sum + num, 0);
+//   const missingLetterCode = expectedSum - actualSum;
+//   return String.fromCharCode(missingLetterCode);
+// }
+function findMissingLetter(letters) {
+  if (!letters || !letters.length) return '';
+  for (let i = 0; i < letters.length; i++) {
+    const current = letters[i].charCodeAt(0);
+    const next = letters[i + 1].charCodeAt(0);
+    if (next - current > 1) {
+      return String.fromCharCode(current + 1);
+    }
+  }
+  return '';
+}
 
 module.exports = findMissingLetter;
