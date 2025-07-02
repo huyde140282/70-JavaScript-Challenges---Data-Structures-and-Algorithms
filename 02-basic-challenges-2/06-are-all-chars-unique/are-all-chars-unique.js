ca1c9c498cc5fabab1fa13ca34e0c81f0089658a
Huy Nguyen <beshin81@gmail.com>
2025-07-02 21:00:01 +0700

    Implement areAllCharactersUnique function and add corresponding tests

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//   if (!str) return true;
+//   const charSet = new Set(str);
+//   return charSet.size === str.length;
+// }
+
+function areAllCharactersUnique(str) {
+  if (!str) return true;
+  const charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) return false;
+    charCount[char] = true;
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
